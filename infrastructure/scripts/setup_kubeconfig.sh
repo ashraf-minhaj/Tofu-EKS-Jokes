acbfdf8c6e72e8c7a2fd01d755826830056b7462
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2024-02-15 00:21:34 +0600

    [docs](script): add necessary scripts to get started

--- infrastructure/scripts/setup_kubeconfig.sh
+++ infrastructure/scripts/setup_kubeconfig.sh
@@ -1,5 +1,10 @@
-# rm .kube/config
+# build image 
+docker buildx build --platform linux/amd64 -t ashraftheminhaj/jokes:v1 -f Dockerfile .
+echo "TOKEN" | sudo docker login --username=ashraftheminhaj --password-stdin
+docker login -u ashraftheminhaj
 
+
+# rm .kube/config
 # get contexts
 kubectl config current-context
 kubectl config get-clusters
@@ -8,7 +13,10 @@ kubectl config get-clusters
 kubectl config get-contexts
 
 # add a new context
+# why do we have to update it everytime?
 aws eks update-kubeconfig --region ap-south-1 --name eks-jokes-app-cluster-dev
 
 # use the context
 kubectl config use-context arn:aws:eks:ap-south-1:408498059527:cluster/eks-jokes-app-cluster-dev
+
+# then kubectl apply -f manifest.yaml
